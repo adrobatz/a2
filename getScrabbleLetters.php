3cf5edabc4ca023c80098ee6b9f2b9176ad22e05
Alyssa Drobatz <drobatzalyssa@gmail.com>
2017-02-25 10:40:11 -0500

    array solved

--- getScrabbleLetters.php
+++ getScrabbleLetters.php
@@ -2,9 +2,36 @@
 
 require('tools.php');
 
-$scrabbleLetters = file_get_contents('scrabble.php');
+$scrabbleLetters = [
+	'a' => '1',
+	'b' => '3',
+	'c' => '3',
+	'd' => '2',
+	'e' => '1',
+	'f' => '4',
+	'g' => '2',
+	'h' => '4',
+	'i' => '1',
+	'j' => '8',
+	'k' => '5',
+	'l' => '1',
+	'm' => '3',
+	'n' => '1',
+	'o' => '1',
+	'p' => '3',
+	'q' => '10',
+	'r' => '1',
+	's' => '1',
+	't' => '1',
+	'u' => '1',
+	'v' => '4',
+	'w' => '4',
+	'x' => '8',
+	'y' => '4',
+	'z' => '10'
 
-$haveResults = true;
+];
+
 
 if(isset($_GET['word'])) {
 	$word = $_GET['word'];
@@ -13,5 +40,47 @@ if(isset($_GET['word'])) {
 		$word = "";
 	}
 dump($word);
+
+// $results = "";
+// for ($i=0; $i < strlen($word); ++$i) { 
+// 	$results .= $i." ";
+// }
+
+// dump($results);
+
+// var sum = 0;
+// for (var i = 0; i < word.length; ++i) {
+//     sum += scores[word.charAt(i)] || 0;
+// }
+
+// alert(sum);
+
+$wordEntry = str_split($word);
+dump($wordEntry);
+
+$flipped = array_flip($wordEntry);
+dump($flipped);
+$intersect = array_intersect_key($scrabbleLetters, $flipped);
+
+dump($intersect);
+
+echo array_sum($intersect);
+
+
+// function addtiles($newWord) {
+
+// }
+
+// print_r(array_filter($scrabbleLetters, "addPoints"));
+
+//echo array_sum($scrabbleLetters);
+
+//echo array_sum($wordEntry);
+
+
+//array_search($word, $scrabbleLetters);
+
+
+
 
 
